a22526c30126cbb1bddfceb93de9a0c5f5b0e056
Lordcobcob <105743637+Lordcobcob@users.noreply.github.com>
2022-11-22 16:50:10 +0000

    Added support for preview.duolingo.com and removed the issue.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,25 @@
 function expandMenu(){
-  console.log("Storys Tab Re-Added!");
+
+
+  var sidebar = document.querySelector("#root > div._1R67g._3YKTw");
   var menu = document.querySelector("#root > div._1R67g._3YKTw > div > div._1JSRd._3SuOC > div._3bTT7");
+
+  // Reload page if its inside a lesson and the logout button is still there
+  if (window.location.href.indexOf('/lesson') !== -1){
+    if (document.body.innerHTML.indexOf('<div class="_1JSRd _3SuOC logout">') !== -1){
+      location.reload();
+    }
+    return;
+  }
+
+  // Exit function if the Stories button is already there to not keep re-adding it
+  if (document.body.innerHTML.indexOf('<span class="_1lJDk">Stories</span>') !== -1){
+      return;
+  }
+
+
+
+  console.log("Storys Tab Re-Added!");
   menu.innerHTML += '<div class="_3LfJs _2ulLR"><a class="_3zmPR" href="./stories" rel="noopener noreferrer noopener" target="/stories" tabindex="0"><span class="_3BxbA _2q30B _23V08 _1R__D _1eTnJ"><div class="AMXUp"><img class="ZFBAG" src="https://static.wikia.nocookie.net/duolingo/images/9/9a/Stories-new.svg"></div><span class="_1lJDk">Stories</span></span></a></div>';
   if (window.location.href.indexOf('/stories') !== -1) {
   
@@ -28,8 +47,6 @@ function expandMenu(){
 
       });
     }
-
-    var sidebar = document.querySelector("#root > div._1R67g._3YKTw");
 
     var logout = `
     <div class="_1JSRd _3SuOC help">
@@ -67,4 +84,27 @@ function expandMenu(){
 
 }
 
-expandMenu()
+setInterval(function() {
+  expandMenu();
+}, 1000);
+
+function espanolTipsLoad() {
+  var espanolTips = `
+  <div class="card">
+    <div class="container">
+      <h4 class=TipsTitle><b>Happy Endings</b></h4>
+      <p>The basic form of a verb is called an infinitive. For example, hablar (to speak), comer (to eat), and escribir (to write) are all infinitives.
+
+        Spanish has three verb groups: verbs ending in ‑ar, ‑er, or ‑ir. These are the ending patterns for each group.</p>
+    </div>
+  </div>
+  `
+
+  var ContentToReplaceSpanish = document.querySelector("#root > div._1R67g._3YKTw > div._2_9xr > div:nth-child(2) > div._2rYrl > div._33Mo9 > div > div:nth-child(3)");
+  ContentToReplaceSpanish.innerHTML = espanolTips;
+}
+
+
+if (window.location.href.indexOf('/guidebook/es/1') !== -1) {
+  espanolTipsLoad();
+}
